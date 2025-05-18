a0527757c452c52604fffcb4526d6b7c06f2e997
Тарас Квасниця <taraskvasnica00@gmail.com>
2025-05-18 17:18:23 +0300

    update filteredMovies in MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -4,9 +4,22 @@ import MovieCard from './MovieCard'
 const MovieList = ({ movies }) => {
 	const [searchTerm, setSearchTerm] = useState('')
 
-	const filteredMovies = movies.filter(movie =>
-		movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-	)
+	const filteredMovies = movies
+		.filter(movie =>
+			movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+		)
+		.sort((a, b) => {
+			// Сортуємо за позицією збігу у назві
+			const aIndex = a.title.toLowerCase().indexOf(searchTerm.toLowerCase())
+			const bIndex = b.title.toLowerCase().indexOf(searchTerm.toLowerCase())
+
+			// Якщо збіг на початку назви - вище в результатах
+			if (aIndex < bIndex) return -1
+			if (aIndex > bIndex) return 1
+
+			// Якщо однакова позиція - сортуємо за довжиною назви
+			return a.title.length - b.title.length
+		})
 
 	return (
 		<div className='movie-list-container'>
@@ -21,14 +34,18 @@ const MovieList = ({ movies }) => {
 
 			<div className='movie-columns'>
 				<div className='movie-column'>
-					{filteredMovies.slice(0, 4).map(movie => (
-						<MovieCard key={movie.id} movie={movie} />
-					))}
+					{filteredMovies
+						.slice(0, Math.ceil(filteredMovies.length / 2))
+						.map(movie => (
+							<MovieCard key={movie.id} movie={movie} />
+						))}
 				</div>
 				<div className='movie-column'>
-					{filteredMovies.slice(4, 8).map(movie => (
-						<MovieCard key={movie.id} movie={movie} />
-					))}
+					{filteredMovies
+						.slice(Math.ceil(filteredMovies.length / 2))
+						.map(movie => (
+							<MovieCard key={movie.id} movie={movie} />
+						))}
 				</div>
 			</div>
 		</div>

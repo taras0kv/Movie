1cb47b3c9a69bef489018593bb35c3557d8fc767
Тарас Квасниця <taraskvasnica00@gmail.com>
2025-05-24 19:58:14 +0300

    update Booking

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
 import CinemaHall from '../components/CinemaHall'
+import BookingForm from '../components/BookingForm'
+import { ToastContainer } from 'react-toastify'
 
 const Booking = () => {
 	const { id } = useParams()
+	const [selectedSeats, setSelectedSeats] = useState([])
+	const [showForm, setShowForm] = useState(false)
+
+	const handleBookingSuccess = () => {
+		setSelectedSeats([])
+		setShowForm(false)
+	}
 
 	return (
 		<div className='booking-page'>
-			<h2>Бронювання місць</h2>
-			<CinemaHall movieId={id} /> {}
+			<CinemaHall movieId={id} />
+
+			{selectedSeats.length > 0 && !showForm && (
+				<button onClick={() => setShowForm(true)}>
+					Забронювати вибрані місця
+				</button>
+			)}
+
+			{showForm && (
+				<BookingForm
+					movieId={id}
+					selectedSeats={selectedSeats}
+					onSuccess={handleBookingSuccess}
+				/>
+			)}
+
+			<ToastContainer />
 		</div>
 	)
 }
-
-export default Booking
